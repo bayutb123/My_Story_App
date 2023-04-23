f21abf059bd225667ccad1a912f1891946b91357
bayutb123 <bayutantra28@gmail.com>
2023-04-23 17:45:12 +0700

    Penambahan Toast jika story gagal dimuat

--- app/src/main/java/com/bayutb/mystoryapp/data/StoryListViewModel.kt
+++ app/src/main/java/com/bayutb/mystoryapp/data/StoryListViewModel.kt
@@ -2,6 +2,7 @@ package com.bayutb.mystoryapp.data
 
 import android.app.Application
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -26,6 +27,7 @@ class StoryListViewModel(application: Application) : AndroidViewModel(applicatio
             }
 
             override fun onFailure(call: Call<StoryListResponse>, t: Throwable) {
+                Toast.makeText(getApplication(), "Failed to retrieve stories", Toast.LENGTH_SHORT).show()
                 Log.d("Failure: ", "${t.message}")
             }
         })
